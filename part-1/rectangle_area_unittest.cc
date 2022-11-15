a373bcfe2c876ea460b3b5abe085fc0cc6cd7666
Christian Bonilla <alton77@csu.fullerton.edu>
2022-11-14 21:11:28 -0800

    idk

--- part-1/rectangle_area_unittest.cc
+++ part-1/rectangle_area_unittest.cc
@@ -1,11 +1,21 @@
-
+// Christian Alton bonilla
+// CPSC 120-01
+// 2022-11-14
+// Alton77@csu.fullerton.edu
+// @alton7759
+//
+// Lab 10-01
+// Partners: @annavera38
+//
+// Calculate the area of a triangle defined by length and width.
+//
 /* Do not edit below this line. */
 /* Do not edit below this line. */
 /* Do not edit below this line. */
 
 #include <gtest/gtest.h>
-#include <climits>
 
+#include <climits>
 #include <cstdio>
 #include <future>
 
