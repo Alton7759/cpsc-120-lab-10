6697b5e056fda2c8e081083e220eee3a485f08bb
Christian Bonilla <Alton77@csu.fullerton.edu>
2022-11-14 20:37:52 -0800

    needs work

--- part-1/rectangle_area.cc
+++ part-1/rectangle_area.cc
@@ -7,6 +7,34 @@
 #include "rectangle_area_functions.h"
 
 int main(int argc, char const* argv[]) {
+  std::vector<std::string> arguments(argv, argv + argc);
+  std::vector<std::string> numbers("-", "1", "2", "3", "4", "5", "6"."7", "8",
+                                   "9");
+  if (arguments.size() == 3) {
+    arguments.erase(arguments.begin());
+  } else {
+    std::cout << "Please provide two arguments, a rectangle's length and "
+                 "width.";
+    return 1;
+  }
+  bool isnum = false;
+  for (auto& check : numbers) {
+    isnum = false;
+    if (arguments.at(0).at(0) = numbers) {
+      isnum = true;
+      break;
+    }
+  }
+  for (auto& check2 : numbers) {
+    isnum = false;
+    if (arguments.at(0).at(0)) {
+      isnum = true;
+      break;
+    }
+  }
+  int length{std::stoi(arguments.at(0))};
+  int width{std::stoi(arguments.at(1))};
+  std::cout << RectangleArea(length, width);
   // TODO: convert the command line arguments to a std::vector of std::strings.
   // TODO: Check to make sure you have enough arguments. If you have too few,
   // print an error message and exit.
--- part-1/rectangle_area_functions.cc
+++ part-1/rectangle_area_functions.cc
@@ -1,28 +1,20 @@
-// TODO: Insert your own header
-
+// Julianna Vera
+// CPSC 120-01
+// 14-11-2022
+// annavera38@csu.fullerton.edu
+// @annavera38
+//
+// Lab 10-01
+// Partners: @alton7759
+//
 // Calculate the area of a triangle defined by length and width.
-//
-// The area of a triangle is the product of the length and width.
-//
-// Although rectangles with negative length and width exist, this program
-// is meant to be used to calculate the positive area of rectangles one
-// could encounter in the physical world.
-//
-// Because of this requirement, length and width must be positive
-// integers. Values less than 1 mean that the rectangle has 0 area.
-//
-// Typical usage:
-// \code
-//   int input_length = 13;
-//   int input_width = 17;
-//   int area = RectangleArea(input_length, input_width);
-// \endcode
-//
-// \param length the length of the rectangle
-// \param width the width the rectangle
-// \returns the area of the rectangle defined by length and width
+#include <iostream>
 int RectangleArea(int length, int width) {
   int area{-1};
-  // TODO: Implement the function according to the specifications.
+  area = width * length;
+  if (width < 0 || length < 0) {
+    area = 0;
+  }
+  std::cout << length << " x " << width << " = ";
   return area;
 }
